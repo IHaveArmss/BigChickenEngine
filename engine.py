451b4a386463f99e94b90da1182cbd28d339b231
Tanase <dennisharcu69@gmail.com>
2026-03-28 22:23:20 +0200

    dialogue ui

--- engine.py
+++ engine.py
@@ -67,7 +67,7 @@ class ShaderCache:
 
 
 # ======================================================================
-SCENE_FILE = 'scenes/bosshallway.json'
+SCENE_FILE = 'scenes/cutscene_demo.json'
 PLAY_INTRO = False # Set to False to skip the opening video
 # ======================================================================
 
